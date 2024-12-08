a4060cff2b9eda6ed15adcf525baabf05fb2a6b7
Adrian <adrian.akipi@web.de>
2024-12-08 20:51:27 +0100

    change class name, move events out, add test w sand

--- src/main/java/net/mhgandhi/blockblocker/BlockBlocker.java
+++ src/main/java/net/mhgandhi/blockblocker/BlockBlocker.java
@@ -1,8 +1,6 @@
 package net.mhgandhi.blockblocker;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -10,21 +8,20 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MOD_ID)
-public class ExampleMod
+@Mod(BlockBlocker.MOD_ID)
+public class BlockBlocker
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "blockblocker";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public ExampleMod() {
+    public BlockBlocker() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -61,16 +58,4 @@ public class ExampleMod
         */
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-        }
-    }
 }
---
+++ src/main/java/net/mhgandhi/blockblocker/ClientModEvents.java
@@ -0,0 +1,31 @@
+package net.mhgandhi.blockblocker;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@Mod.EventBusSubscriber(modid = BlockBlocker.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientModEvents {
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        BlockBlocker.LOGGER.info("HELLO FROM CLIENT SETUP");
+        BlockBlocker.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+
+    @SubscribeEvent
+    public void onBlockPlace(BlockEvent.EntityPlaceEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            if (event.getPlacedBlock().getBlock() == Blocks.SAND) {
+                BlockBlocker.LOGGER.info("SAND PLACED");
+                event.setCanceled(true);
+            }
+        }
+    }
+}
